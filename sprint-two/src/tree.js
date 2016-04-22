fdec52cf2e6e570efd425c0480987b2ae9529c74
kqlee <kentqlee@gmail.com>
2016-04-21 22:13:17 -0700

    makes progress on tree datastructure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,26 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = {};
+  newTree.counter = 0;
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = {};
+  child.value = value;
+  child.children = {};
+  _.extend(child, treeMethods);
+  this.children[this.counter] = child;
+  this.counter++;
 };
 
 treeMethods.contains = function(target) {
+  return this.children[this.counter - 1].value === target;
 };
 
 
