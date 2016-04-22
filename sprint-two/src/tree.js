238ccb7d44f246d20fcba48a6f564e6857abaf5f
kqlee <kentqlee@gmail.com>
2016-04-22 12:08:52 -0700

    completes tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,7 @@ var Tree = function(value) {
   newTree.value = value;
   _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = {};
+  newTree.children = {}; // fix me
   newTree.counter = 0;
   return newTree;
 };
@@ -12,15 +12,33 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   var child = {};
-  child.value = value;
-  child.children = {};
   _.extend(child, treeMethods);
+  child.counter = 0;
+  child.children = {};
+
   this.children[this.counter] = child;
+  this.children[this.counter].value = value;
   this.counter++;
 };
 
 treeMethods.contains = function(target) {
-  return this.children[this.counter - 1].value === target;
+  var flag = false;
+
+  if (!flag) {
+    for (var i = 0; i < this.counter; i++) {
+      // console.log('children:' + this.children[i].value);
+      if (this.children[i].value === target) {
+        flag = true;
+        // console.log('children: ' + this.children[i].value + ', flag =' + flag);
+        return flag;
+      } 
+      if (!flag) {
+        flag = this.children[i].contains(target);
+      }
+    }
+  }
+  
+  return flag;
 };
 
 
