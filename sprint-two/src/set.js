386c246f435e69faeb656e5dfa90e846f2ac89f6
kqlee <kentqlee@gmail.com>
2016-04-22 19:40:37 -0700

    Set complete

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  return this[item] === item;
 };
 
 setPrototype.remove = function(item) {
+  delete this[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// This set (being implemented) have constant time lookup because they are objects (objects have constant time lookup)
+// .add - constant time
+// .contains - constant time
+// .remove - constant time
