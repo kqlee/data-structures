8cd00237511fe2e816a7d3d0b3403f7a89eb7bc1
kqlee <kentqlee@gmail.com>
2016-04-23 11:33:29 -0700

    progress on hash table, still fixing collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,31 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  this._storage.set(index, []);
+  var currentBucket = this._storage.get(index);
+  currentBucket.push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var currentBucket = this._storage.get(index);
+  for (var i = 0; i < currentBucket.length; i++) {
+    if (currentBucket[i][0] === k) {
+      return currentBucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var currBucket = this._storage.get(index);
+  for (var i = 0; i < currBucket.length; i++) {
+    if (currBucket[i][0] === k) {
+      currBucket[i][1] = undefined;
+    }
+  }
+  // this._storage.set(index, undefined);
 };
 
 
