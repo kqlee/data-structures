be7f616256196776344742fb2c861c359a24a44f
kqlee <kentqlee@gmail.com>
2016-04-22 20:21:58 -0700

    attempts set implementation using arrays

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,3 +1,4 @@
+//Implemented with Objects
 var Set = function() {
   var set = Object.create(setPrototype);
   set._storage = {};
@@ -26,3 +27,46 @@ setPrototype.remove = function(item) {
 // .add - constant time
 // .contains - constant time
 // .remove - constant time
+
+//Implemented with Arrays
+var Set = function() {
+  var set = Object.create(setPrototype);
+  set._storage = [];
+  return set;
+};
+
+var setPrototype = {};
+
+setPrototype.add = function(item) {
+  this._storage.push(item);
+};
+
+setPrototype.contains = function(item) {
+  var flag = false;
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      flag = true;
+    }
+  }
+  return flag;
+};
+
+setPrototype.remove = function(item) {
+  // _.each(this._storage, function(value, index) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      // return function () {
+      delete this._storage[i];
+      // };
+    }
+  }
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
+
+// This set data structure was implemented with the array data structure so:
+// .add - constant time
+// .contains - linear time
+// .remove - linear time
